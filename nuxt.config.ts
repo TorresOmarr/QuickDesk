3b4c6284aee35da6d68520d4a79ec52c17df774c
Omar Torres <jtorres@axsistec.com>
2025-04-16 20:37:46 -0600

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@nuxt/image',
+    'nuxt-auth-utils'
+  ]
+})
---
+++ server/api/users/stats.get.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(async (event) => {
+    // make sure the user is logged in
+    // This will throw a 401 error if the request doesn't come from a valid user session
+    const { user } = await requireUserSession(event)
+
+    // TODO: Fetch some stats based on the user
+
+    return {
+        user: user,
+        stats: {
+            posts: 10,
+            comments: 20,
+            likes: 30
+        }
+    }
+});
